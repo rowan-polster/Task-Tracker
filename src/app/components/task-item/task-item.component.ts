8dade7c40be6729c0fae0e18b61c67133d4fa646
Rowan Polster <rowandownriver94@gmail.com>
2021-10-14 16:23:30 -0500

    added delete icon to task list

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Task} from '../../Task';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -10,6 +11,8 @@ export class TaskItemComponent implements OnInit {
 
   @Input()
   task!: Task;
+
+  faTimes = faTimes;
 
   constructor() { }
 
