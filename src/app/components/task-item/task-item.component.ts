06f2e66350e161f674565894b3dab0c014c0b4bf
Rowan Polster <rowandownriver94@gmail.com>
2021-10-14 15:56:30 -0500

    updated angular???

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Task} from '../../Task';
 
 @Component({
   selector: 'app-task-item',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
+
+  @Input()
+  task!: Task;
 
   constructor() { }
 
