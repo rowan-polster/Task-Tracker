7e2e72fd5f6b1cc1f02fd01c41a57d3de38599ee
Rowan Polster <rowandownriver94@gmail.com>
2021-10-14 20:01:04 -0500

    created fake backend api to store task data

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import {Task} from '../Task';
 import {TASKS} from '../mock-tasks';
@@ -8,10 +9,11 @@ import {TASKS} from '../mock-tasks';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks'
+
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
